7076558e765bd2871c64171826e9790bc98b656b
Ghurtchu <anzori.gurchumelia@gau.edu.ge>
2022-11-20 04:59:17 -0800

    add view for displaying mathy expressions

--- src/main/scala/View.scala
+++ src/main/scala/View.scala
@@ -1,3 +1,7 @@
 object View {
-
+  def apply(expression: Expression): String = expression match {
+    case Expression.Literal(n)  => s"$n"
+    case Expression.Negate(e)   => s"(-${apply(e)})"
+    case Expression.Add(e1, e2) => s"(${apply(e1)} + ${apply(e2)})"
+  }
 }
